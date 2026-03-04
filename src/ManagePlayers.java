5a4990726905d6fab0d722f0bb668a30d87b6f6a
KevinTranbar <kt222sx@student.lnu.se>
2026-03-04 17:19:19 +0100

    Task 1-4

--- src/ManagePlayers.java
+++ src/ManagePlayers.java
@@ -26,23 +26,7 @@ public class ManagePlayers {
         }
     }
 
-    public void showPlayersByNumOfGoals(){/*
-        for (int i = 0; i < players.size() - 1; i++){ //Bubble sort
-            for (int j = 0; j < players.size() - 1 - i; j++){
-                if (players.get(j).getNumOfGoals() < players.get(j + 1).getNumOfGoals()){
-                    Player placeholder = players.get(j);
-                    players.set(j, players.get(j + 1));
-                    players.set(j + 1, placeholder);
-
-                }
-            }
-        }
-
-        for (Player p : players){
-            System.out.println("Player: " + p.getName() + " " + p.getTeam() + " " + p.getPosition() + " " + p.getNumOfGoals());
-        }
-        */
-
+    public void showPlayersByNumOfGoals(){
         Collections.sort(players);
         Collections.reverse(players);
 
